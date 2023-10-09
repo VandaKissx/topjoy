e946607afb1fa6fd87fa4fcbfb95a4c4bbbb2618
diak <diak@mesterlevel.lo>
2023-10-09 12:46:02 +0200

    new version

--- lib/controllers/home_view_controller.dart
+++ lib/controllers/home_view_controller.dart
@@ -1,9 +1,15 @@
 import 'dart:math';
 
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:topjoy/constants.dart' as constants;
 
 class HomeViewController extends GetxController{
+  BuildContext context;
+  HomeViewController(this.context);
+
+  bool isInitialized=false;
+
   int bottlecupstate = 0;
   int random = 0;
 
@@ -11,6 +17,7 @@ class HomeViewController extends GetxController{
     int random = Random().nextInt(constants.messages.length);
     return (bottlecupstate==10)?constants.messages[random].toUpperCase():"";
   }
+  
   void flipTheBottlecap() async {
     bool condition =(bottlecupstate == 0);
     random= Random().nextInt(constants.messages.length);
@@ -20,5 +27,23 @@ class HomeViewController extends GetxController{
        await Future.delayed(Duration(milliseconds: 15));
       update();
     }
+  }
+  Future<void> preloadAllImages()  async{
+    for (int i = 0; i<11; i++){
+      await(AssetImage('images/bottlecap_$i.png'),context);
+
+    }
+
+  }
+
+  @override
+  void onInit() async {
+    // TODO: implement onInit
+    super.onInit();
+    await preloadAllImages();
+    isInitialized = true;
+    //await Future.delayed(Duration(milliseconds: 2000));
+    update();
+
   }
 }
